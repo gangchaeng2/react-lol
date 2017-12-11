fc8a298718b8531e668d06324ec42f9d1442522f
gangchang <ktw6750@hanmail.net>
2017-12-11 19:59:34 +0900

    syntanx error modify

--- src/services/search.js
+++ src/services/search.js
@@ -17,41 +17,41 @@ export function getCurrentStatus(summonerId) {
 
 // 게임 리스트 상세정보
 export async function getGameListInfo(list) {
-  const promises = [];
-  let matchList =[];
+    let promises = [];
+    let matchList =[];
 
-  list.map((match, i) => {
-      promises.push(axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matches/${match.gameId}?api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`));
-  });
+    list.forEach((match, i) => {
+        promises.push(axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matches/${match.gameId}?api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`));
+    });
 
-  await axios.all(promises)
-  .then(function(results){
-    results.forEach(function(response) {
-        matchList = matchList.concat(response.data);
-      })
-  })
-  .catch(function(error){
-      alert('요청이 너무 많습니다. 잠시후 다시 검색해주세요');
-  });
+    await axios.all(promises)
+    .then(function(results){
+      results.forEach(function(response) {
+          matchList = matchList.concat(response.data);
+        });
+    })
+    .catch(function(error){
+        alert('요청이 너무 많습니다. 잠시후 다시 검색해주세요');
+    });
 
-  return matchList;
+    return matchList;
 }
 
 // 게임 리스트
 export function getMatchList(championId, accountId) {
-  if(championId === 'recent' || championId === '전체') {
-      return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}/recent?api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
-  } else if(championId === '일반게임'){
-      // 일반게임 최근 20경기
-      return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?queue=430&endIndex=20&season=9&api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
-  } else if(championId === '솔로랭크'){
-      // 솔로랭크 최근 20경기
-      return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?queue=420&endIndex=20&season=9&api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
-  } else if(championId === '자유랭크'){
-      // 솔로랭크 최근 20경기
-      return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?queue=440&endIndex=20&season=9&api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
-  } else {
-      // 챔피언에 대한 최근 20경기
-      return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?champion=${championId}&endIndex=20&api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
-  }
+    if(championId === 'recent' || championId === '전체') {
+        return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}/recent?api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
+    } else if(championId === '일반게임'){
+        // 일반게임 최근 20경기
+        return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?queue=430&endIndex=20&season=9&api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
+    } else if(championId === '솔로랭크'){
+        // 솔로랭크 최근 20경기
+        return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?queue=420&endIndex=20&season=9&api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
+    } else if(championId === '자유랭크'){
+        // 솔로랭크 최근 20경기
+        return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?queue=440&endIndex=20&season=9&api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
+    } else {
+        // 챔피언에 대한 최근 20경기
+        return axios.get(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}?champion=${championId}&endIndex=20&api_key=RGAPI-97fa050e-afd2-4dd0-99b0-2e79fe32ac9f`);
+    }
 }
