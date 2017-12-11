9b8eec529ca7c242ec91c7657633506509669a7f
gangchang <ktw6750@hanmail.net>
2017-12-11 16:43:59 +0900

    keyPress and autocomplete remove

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,26 @@ import React from 'react';
 
 import './Header.css';
 
-const Header = (fn) => (
-    <div className="Header">
-        <div className="ui action input">
-            <input type="text" id="summonerName" name="summonerName" placeholder="소환사명" onChange={fn.handleChange} />
-            <button className="ui button" onClick={fn.searchSummoner}>검색</button>
-        </div>
-    </div>
-)
+const Header = ({ searchSummoner, handleChange, summonerName }) => {
+    const pressSearch = (e) => {
+        if(e.charCode === 13) {
+            searchSummoner(e.target.value);
+        }
+    }
+
+    const search = () => {
+        const summonerName = document.getElementById('summonerName').value;
+        searchSummoner(summonerName);
+    }
+
+    return (
+      <div className="Header">
+          <div className="ui action input">
+              <input type="text" id="summonerName" name="summonerName" placeholder="소환사명" onKeyPress={pressSearch} onChange={handleChange} autoComplete='off' value={summonerName}/>
+              <button className="ui button" onClick={search}>검색</button>
+          </div>
+      </div>
+    );
+}
 
 export default Header;
