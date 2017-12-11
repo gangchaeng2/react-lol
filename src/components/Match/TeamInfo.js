60ad362db773cd3e65fc84769df8f2625bb53ccf
gangchang <ktw6750@hanmail.net>
2017-12-11 16:46:53 +0900

    state change

--- src/components/Match/TeamInfo.js
+++ src/components/Match/TeamInfo.js
@@ -4,10 +4,10 @@ import { Image } from 'semantic-ui-react';
 const TeamInfo = ({ team }) =>  {
     return (
           <tr>
-            {team.map((championId, i) => {
+            {team.map((teamInfo, i) => {
                 return (
                   <td key={i}>
-                    <Image src={`http://iplol.co.kr/images/champ_2015/${championId}.png`} shape='rounded' size='mini' />
+                    <Image src={`http://iplol.co.kr/images/champ_2015/${teamInfo.championId}.png`} shape='rounded' size='mini' />
                   </td>
                 );
             })}
