386943ad8df2c97f62c57398d025e2a3780c7db1
gangchang <ktw6750@hanmail.net>
2017-12-19 18:53:13 +0900

    summonerInfo rank modfiy

--- src/components/SummonerInfo/SummonerInfo.js
+++ src/components/SummonerInfo/SummonerInfo.js
@@ -3,37 +3,34 @@ import { Item } from 'semantic-ui-react';
 
 import * as utils from '../../services/utils';
 
-const rankItem = (rankInfoList) => {
-    let rankInfoChange = [];
-    if(rankInfoList.length > 1) {
-        rankInfoChange = rankInfoChange.concat(rankInfoList[1]).concat(rankInfoList[0]);
-    } else {
-        rankInfoChange = rankInfoList;
-    }
+const rankItem = (rankInfo) => {
+    const tireSrc = `//opgg-static.akamaized.net/images/medals/${rankInfo[0].tier.toLowerCase()}_${utils.getTierNum(rankInfo[0].rank)}.png`;
 
-    return rankInfoChange.map((rankInfo, i) => {
-        const tireSrc = `//opgg-static.akamaized.net/images/medals/${rankInfo.tier.toLowerCase()}_${utils.getTierNum(rankInfo.rank)}.png`;
+    return(
+      <Item>
+        <Item.Image size='small' src={tireSrc} />
 
-        return(
-          <Item key={i}>
-            <Item.Image size='small' src={tireSrc} />
-
-            <Item.Content verticalAlign='middle'>
-              <Item.Header>{rankInfo.queueType === 'RANKED_FLEX_SR' ? '자유랭크' : '솔로랭크'}</Item.Header><br/>
-              <Item.Header>{rankInfo.tier} {rankInfo.rank}</Item.Header>
-              <Item.Meta>
-                <span className='cinema'>{rankInfo.leagueName}</span>
-              </Item.Meta>
-              <Item.Description>{rankInfo.leaguePoints}LP / {rankInfo.wins}승 {rankInfo.losses}패</Item.Description>
-              <Item.Description><b>승률 : {(rankInfo.wins/(rankInfo.wins+rankInfo.losses)*100).toFixed(2)}%</b></Item.Description>
-            </Item.Content>
-          </Item>
-        );
-    });
+        <Item.Content verticalAlign='middle'>
+          <Item.Header>{rankInfo[0].queueType === 'RANKED_FLEX_SR' ? '자유랭크' : '솔로랭크'}</Item.Header><br/>
+          <Item.Header>{rankInfo[0].tier} {rankInfo[0].rank}</Item.Header>
+          <Item.Meta>
+            <span className='cinema'>{rankInfo[0].leagueName}</span>
+          </Item.Meta>
+          <Item.Description>{rankInfo[0].leaguePoints}LP / {rankInfo[0].wins}승 {rankInfo[0].losses}패</Item.Description>
+          <Item.Description><b>승률 : {(rankInfo[0].wins/(rankInfo[0].wins+rankInfo[0].losses)*100).toFixed(2)}%</b></Item.Description>
+        </Item.Content>
+      </Item>
+    );
 };
 
 const SummonerInfo = ({summonerInfo, summonerRankInfo, summonerStatus, summonerName}) => {
     const lastLogin = new Date(summonerInfo.revisionDate).toLocaleString();
+    const soloRank = summonerRankInfo.filter(function(data){
+      return data.queueType === "RANKED_SOLO_5x5";
+    });
+    const freeRank = summonerRankInfo.filter(function(data){
+      return data.queueType === "RANKED_FLEX_SR";
+    });
 
     return (
       <Item.Group divided>
@@ -47,7 +44,12 @@ const SummonerInfo = ({summonerInfo, summonerRankInfo, summonerStatus, summonerN
             <Item.Description>{summonerStatus ? "현재 게임중 입니다." : "현재 게임중이 아닙니다."}</Item.Description>
           </Item.Content>
         </Item>
-        {rankItem(summonerRankInfo)}
+        {soloRank.length > 0 &&
+          rankItem(soloRank)
+        }
+        {freeRank.length > 0 &&
+          rankItem(freeRank)
+        }
       </Item.Group>
     )
 }
